7222f646a8ccd2c14a56a5373189c3a28d29f36a
Justin Sovine <justinsovine@gmail.com>
2024-10-30 10:18:03 -0400

    Lesson 12: Page Links

--- app/about.view.php
+++ app/about.view.php
@@ -121,12 +121,12 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Us</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <p>Hello. Welcome to the homepage.</p>
+                <p>Now you are on the About page.</p>
             </div>
         </main>
     </div>
--- app/contact.view.php
+++ app/contact.view.php
@@ -121,12 +121,12 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact Us</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <p>Hello. Welcome to the homepage.</p>
+                <p>Contact us now!</p>
             </div>
         </main>
     </div>
