25e1a2cfc1f8e0c2b80ce38e2636bafb43e2bd02
Justin Sovine <justinsovine@gmail.com>
2024-10-31 22:25:06 -0400

    SQL Injection Vulnerabilities Explained

--- app/index.php
+++ app/index.php
@@ -5,9 +5,11 @@ require 'Database.php';
 //require 'router.php';
 
 $config = require 'config.php';
-
 $db = new Database($config['database'], getenv('DB_USER'), getenv('DB_PASSWORD'));
-$query = "SELECT * FROM posts WHERE id = 1";
-$post = $db->query($query)->fetch();
 
-dd($post['title']);
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
+
+dd($posts);
