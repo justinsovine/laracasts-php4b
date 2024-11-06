85b15e87955fd0caf9a97d1f4591379744025b10
Justin Sovine <justinsovine@gmail.com>
2024-11-05 20:21:44 -0500

    Intro to Form Validation

--- app/controllers/note-create.php
+++ app/controllers/note-create.php
@@ -6,11 +6,21 @@ $db = new Database($config['database'], getenv('DB_USER'), getenv('DB_PASSWORD')
 $heading = "Create Note";
 
 if ($_POST) {
-    $query = 'INSERT INTO notes(body, user_id) VALUES(:body, :user_id)';
-    $notes = $db->query($query, [
-        ':body' => $_POST['body'],
-        ':user_id' => 1
-    ])->get();
+    $errors = [];
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required.';
+    }
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body can not be more than 1,000 characters.';
+    }
+
+    if (empty($errors)) {
+        $query = 'INSERT INTO notes(body, user_id) VALUES(:body, :user_id)';
+        $notes = $db->query($query, [
+            ':body' => $_POST['body'],
+            ':user_id' => 1
+        ])->get();
+    }
 }
 
 require "views/note-create.view.php";
